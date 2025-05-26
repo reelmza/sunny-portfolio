e87e0c029a82a3dfdea264dc991631ed33aea57c
reelmza <jessemoses71@gmail.com>
2025-05-26 21:50:21 +0100

    Framer Changes

--- src/components/Lilcon.jsx
+++ src/components/Lilcon.jsx
@@ -3,7 +3,7 @@ import { motion, useScroll } from "motion/react";
 const Lilcon = ({ reference }) => {
   const { scrollYProgress } = useScroll({
     target: reference,
-    offset: ["center end", "center center"],
+    offset: ["end end", "end center"],
   });
   return (
     <figure className="absolute left-0 fill-white stroke-gray-800 scale-75 lg:scale-75">
@@ -27,7 +27,7 @@ const Lilcon = ({ reference }) => {
           cx={"75"}
           cy={"50"}
           r={"20"}
-          className="stroke-[5px] stroke-gray-800"
+          className="stroke-[4px] stroke-gray-800"
         />
 
         <circle
