623ee13403b35c428f0c05dedf3dbbcdc6e11952
reelmza <jessemoses71@gmail.com>
2025-07-07 19:46:46 +0100

    Updated education and skills

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,6 +3,8 @@ export const About = () => {
 
   const backendSkills = ["Python", "JavaScript", "PHP", "C#"];
 
+  const gameSkills = ["Unity", "Unreal Engine", "Blender", "RPG Maker MZ"];
+
   return (
     <section
       id="about"
@@ -23,6 +25,22 @@ export const About = () => {
           </p>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="rounded-xl p-0 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Game Development</h3>
+              <div className="flex flex-wrap gap-2">
+                {gameSkills.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm cursor-pointer hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+
             <div className="rounded-xl p-0 hover:-translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4"> Frontend</h3>
               <div className="flex flex-wrap gap-2">
@@ -62,8 +80,8 @@ export const About = () => {
             <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
             <ul className="list-disc list-inside space-y-2">
               <li>
-                <strong> MSc. in Computing </strong> - University of Roehampton
-                (2024-2025)
+                <strong> MSc. in Computing </strong> - University of Roehampton,
+                London (2024-2025)
               </li>
               <li>
                 Game Design, Software Development, AR/VR Technologies, Web
